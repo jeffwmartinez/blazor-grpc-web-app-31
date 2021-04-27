75085822e3888a0da1bc4a42ec12a25e15ace620
Jeff Martinez <jeffxmartinez@gmail.com>
2021-04-27 15:44:47 -0700

    Add project files.

---
+++ BlazorGrpcWebApp31/Shared/WeatherForecast.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BlazorGrpcWebApp31.Shared
+{
+    public partial class WeatherForecast
+    {
+        public DateTime Date { get; set; }
+
+        public int temperatureC { get; set; }
+
+        public string summary { get; set; }
+
+        // public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+    }
+}
---
+++ BlazorGrpcWebApp31/Shared/WeatherService.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Grpc.Core;
+using Google.Protobuf.WellKnownTypes;
+using System.Threading.Tasks;
+using System.Linq;
+
+namespace BlazorGrpcWebApp31.Shared
+{
+    public class WeatherService : WeatherForecasts.WeatherForecastsBase
+	{
+		private static readonly string[] Summaries = new[]
+		{
+			"Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+		};
+
+		public override Task<WeatherReply> GetWeather(WeatherForecast request, ServerCallContext context)
+		{
+			var reply = new WeatherReply();
+			var rng = new Random();
+
+			reply.Forecasts.Add(Enumerable.Range(1, 10).Select(index => new WeatherForecast
+			{
+				Date = DateTime.Now.AddDays(index),
+				TemperatureC = rng.Next(20, 55),
+				Summary = Summaries[rng.Next(Summaries.Length)]
+			}));
+
+			return Task.FromResult(reply);
+		}
+	}
+}
